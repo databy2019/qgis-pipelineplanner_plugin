5b266027843b2abdb3f8982301171eeb87e19cd6
Wawan Hendriawan Nur <databy2019@gmail.com>
2024-05-27 13:34:35 +0700

    Add table impact for potential pipeline route to raptor, eagle, and buowl

--- pipeline_planner.py
+++ pipeline_planner.py
@@ -21,9 +21,13 @@
  *                                                                         *
  ***************************************************************************/
 """
-from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication
+from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication, Qt
 from qgis.PyQt.QtGui import QIcon
-from qgis.PyQt.QtWidgets import QAction
+from qgis.PyQt.QtWidgets import QAction, QMessageBox, QTableWidgetItem
+
+#ctreated syntax by me
+from qgis.gui import QgsMapToolEmitPoint, QgsRubberBand
+from qgis.core import QgsProject
 
 # Initialize Qt resources from file resources.py
 from .resources import *
@@ -45,9 +49,17 @@ class PipelinerPlanner:
         """
         # Save reference to the QGIS interface
         self.iface = iface
+        self.canvas = self.iface.mapCanvas()
         # initialize plugin directory
         self.plugin_dir = os.path.dirname(__file__)
         # initialize locale
+
+        #ctreated syntax by me to declare
+        self.addPipelinePoint = QgsMapToolEmitPoint(self.canvas)
+        self.rbPipeline = QgsRubberBand(self.canvas)
+        self.rbPipeline.setColor(Qt.red)
+        self.rbPipeline.setWidth(4)
+
         locale = QSettings().value('locale/userLocale')[0:2]
         locale_path = os.path.join(
             self.plugin_dir,
@@ -66,6 +78,12 @@ class PipelinerPlanner:
         # Check if plugin was started the first time in current QGIS session
         # Must be set in initGui() to survive plugin reloads
         self.first_start = None
+
+        # created syntax by me
+        self.dlg = PipelinerPlannerDialog()
+        self.dlg.tblImpacts.setColumnWidth(1,50)
+        self.dlg.tblImpacts.setColumnWidth(2, 225)
+        self.dlg.tblImpacts.setColumnWidth(3, 75)
 
     # noinspection PyMethodMayBeStatic
     def tr(self, message):
@@ -170,6 +188,8 @@ class PipelinerPlanner:
         # will be set False in run()
         self.first_start = True
 
+        # created syntax by me
+        self.addPipelinePoint.canvasClicked.connect(self.evaluatePipeline)
 
     def unload(self):
         """Removes the plugin menu item and icon from QGIS GUI."""
@@ -179,22 +199,68 @@ class PipelinerPlanner:
                 action)
             self.iface.removeToolBarIcon(action)
 
-
     def run(self):
         """Run method that performs all the real work"""
+        self.canvas.setMapTool(self.addPipelinePoint)
 
-        # Create the dialog with elements (after translation) and keep reference
-        # Only create GUI ONCE in callback, so that it will only load when the plugin is started
-        if self.first_start == True:
-            self.first_start = False
-            self.dlg = PipelinerPlannerDialog()
+    def evaluatePipeline(self, point, button):
+        if button == Qt.LeftButton:
+            self.rbPipeline.addPoint(point)
+            self.rbPipeline.show()
 
-        # show the dialog
-        self.dlg.show()
-        # Run the dialog event loop
-        result = self.dlg.exec_()
-        # See if OK was pressed
-        if result:
-            # Do something useful here - delete the line containing pass and
-            # substitute with your code.
-            pass
+        elif button == Qt.RightButton:
+            pipeline = self.rbPipeline.asGeometry()
+            #reset table to empty
+            self.dlg.tblImpacts.setRowCount(0)
+
+            #QMessageBox.information(None, "Pipeline", pipeline.asWkt())
+            ##Raptor
+            lyrRaptor = QgsProject.instance().mapLayersByName("Raptor Buffer")[0]
+            raptors = lyrRaptor.getFeatures(pipeline.boundingBox())
+            for raptor in raptors:
+                valConstraint = raptor.attribute("recentspec")
+                valID = raptor.attribute("Nest_ID")
+                valStatus = raptor.attribute("recentstat")
+                valDistance = pipeline.distance(raptor.geometry().centroid())
+                if raptor.geometry().intersects(pipeline):
+                    row = self.dlg.tblImpacts.rowCount()
+                    self.dlg.tblImpacts.insertRow(row)
+                    self.dlg.tblImpacts.setItem(row, 0, QTableWidgetItem(valConstraint))
+                    self.dlg.tblImpacts.setItem(row, 1, QTableWidgetItem(str(valID)))
+                    self.dlg.tblImpacts.setItem(row, 2, QTableWidgetItem(valStatus))
+                    self.dlg.tblImpacts.setItem(row, 3, QTableWidgetItem("{:4.5}".format(valDistance)))
+
+            ##Eagle
+            lyrEagle = QgsProject.instance().mapLayersByName("BAEA Buffer")[0]
+            eagles = lyrEagle.getFeatures(pipeline.boundingBox())
+            for eagle in eagles:
+                valConstraint = "BAEA Nest"
+                valID = eagle.attribute("nest_id")
+                valStatus = eagle.attribute("status")
+                valDistance = pipeline.distance(eagle.geometry().centroid())
+                if eagle.geometry().intersects(pipeline):
+                    row = self.dlg.tblImpacts.rowCount()
+                    self.dlg.tblImpacts.insertRow(row)
+                    self.dlg.tblImpacts.setItem(row, 0, QTableWidgetItem(valConstraint))
+                    self.dlg.tblImpacts.setItem(row, 1, QTableWidgetItem(str(valID)))
+                    self.dlg.tblImpacts.setItem(row, 2, QTableWidgetItem(valStatus))
+                    self.dlg.tblImpacts.setItem(row, 3, QTableWidgetItem("{:4.5}".format(valDistance)))
+
+            ##Buowl
+            lyrBUOWL = QgsProject.instance().mapLayersByName("BUOWL Buffer")[0]
+            buowls = lyrBUOWL.getFeatures(pipeline.boundingBox())
+            for buowl in buowls:
+                valConstraint = "BAUOWL Habitat"
+                valID = buowl.attribute("habitat_id")
+                valStatus = buowl.attribute("recentstat")
+                valDistance = pipeline.distance(buowl.geometry().buffer(-0.001, 5))
+                if buowl.geometry().intersects(pipeline):
+                    row = self.dlg.tblImpacts.rowCount()
+                    self.dlg.tblImpacts.insertRow(row)
+                    self.dlg.tblImpacts.setItem(row, 0, QTableWidgetItem(valConstraint))
+                    self.dlg.tblImpacts.setItem(row, 1, QTableWidgetItem(str(valID)))
+                    self.dlg.tblImpacts.setItem(row, 2, QTableWidgetItem(valStatus))
+                    self.dlg.tblImpacts.setItem(row, 3, QTableWidgetItem("{:4.5}".format(valDistance)))
+
+            self.dlg.show()
+            self.rbPipeline.reset()
